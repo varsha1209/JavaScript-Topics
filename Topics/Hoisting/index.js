15d5bc62f6e229b6c68feb03dc0839c284d6caee
varsha dahare <varsha.dahare@ziroh.com>
2023-12-09 01:42:58 +0530

    basics of hoisting

---
+++ Topics/Hoisting/index.js
@@ -0,0 +1,24 @@
+//Part 1 
+// console.log(myName);
+// var myName = "Varsha";
+
+// function getName() {
+// console.log("hy")
+// }
+
+// console.log(myName)
+
+// getName(); 
+// output is = undefined , Varsha , hy
+
+//Part 2
+// console.log(getName) //undefined
+// console.log(getName()); //getName is not function
+// var myName = "Varsha";
+
+// var getName = () => {
+// console.log("hy")
+// } // this is behave like a variable 
+
+// console.log(myName);
+
