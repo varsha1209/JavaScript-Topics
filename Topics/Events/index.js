d35a6b302c443f24f81dd56c9d8bcef2d369e342
varsha dahare <varsha.dahare@ziroh.com>
2023-12-26 17:07:05 +0530

    added event bubbling example

---
+++ Topics/Events/index.js
@@ -0,0 +1,15 @@
+const grandParent = document.querySelector(".grandParent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+grandParent.addEventListener("click", () => {
+    alert("grandParent");
+})
+
+parent.addEventListener("click", () => {
+    alert("parent");
+})
+
+child.addEventListener("click", () => {
+    alert("child");
+})
